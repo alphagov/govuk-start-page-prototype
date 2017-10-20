d9c862cb41c30a1983455d42a33ca835fc17f70e
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2017-10-20 11:22:20 +0100

    Add options-on-interstitial/check-state-pension-interstitial-1 page
    We also add a route to handle the post request submitted by the form.

--- app/routes.js
+++ app/routes.js
@@ -7,5 +7,14 @@ router.get('/', function (req, res) {
 })
 
 // add your routes here
+router.post('/check-state-pension-options-1', function (req, res) {
+  if (req.body.loginoption === "government-gateway") {
+    res.redirect('https://www.tax.service.gov.uk/gg/sign-in?continue=%2Fcheck-your-state-pension%2Faccount&origin=nisp-frontend&accountType=individual')
+  } else if (req.body.loginoption === "verify") {
+    res.redirect('https://www.tax.service.gov.uk/check-your-state-pension/signin/verify')
+  } else {
+    res.redirect('/')
+  }
+})
 
 module.exports = router
