b3570554be872a3a128c7edd5c151344c0137e48
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2017-10-24 15:26:13 +0100

    Add error message to check-state-pension-interstitial-1
    If a user does not select an option from the list of sign in options, we now
    display an error requiring them to select an option.

--- app/routes.js
+++ app/routes.js
@@ -12,8 +12,10 @@ router.post('/check-state-pension-options-1', function (req, res) {
     res.redirect('https://www.tax.service.gov.uk/gg/sign-in?continue=%2Fcheck-your-state-pension%2Faccount&origin=nisp-frontend&accountType=individual')
   } else if (req.body.loginoption === "verify") {
     res.redirect('https://www.tax.service.gov.uk/check-your-state-pension/signin/verify')
-  } else {
+  } else if (req.body.loginoption === "no-account") {
     res.redirect('/options-on-interstitial/create-new-account-1')
+  } else {
+    res.render('options-on-interstitial/check-state-pension-interstitial-1', { error: true })
   }
 })
 
