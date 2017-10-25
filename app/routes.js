405c4d9f40fc1ee694b709799a76c011d30efa98
Nick Colley <nick.colley@digital.cabinet-office.gov.uk>
2017-10-25 15:02:12 +0100

    Update content
    Made some tweaks based on Tom and Kirsty's input
    
    - Removed logos on B1
    - Moved create an account to by the side of the buttons

--- app/routes.js
+++ app/routes.js
@@ -11,7 +11,7 @@ router.post('/options-on-interstitial/check-state-pension-interstitial-1', funct
   if (req.body.loginoption === "government-gateway") {
     res.redirect('https://www.tax.service.gov.uk/gg/sign-in?continue=%2Fcheck-your-state-pension%2Faccount&origin=nisp-frontend&accountType=individual')
   } else if (req.body.loginoption === "verify") {
-    res.redirect('https://www.tax.service.gov.uk/check-your-state-pension/signin/verify')
+    res.redirect('https://www.signin.service.gov.uk/start')
   } else if (req.body.loginoption === "no-account") {
     res.redirect('/options-on-interstitial/create-new-account-1')
   } else {
